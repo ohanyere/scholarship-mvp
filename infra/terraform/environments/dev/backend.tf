696451e1d76fcca992ae66acd6b614c6dae4b9aa
Ohanyere Francis <mooref068@gmail.com>
2026-04-14 14:28:35 -0700

    chore: configure Terraform S3 backend for dev environment

---
+++ infra/terraform/environments/dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "scholarship-mvp-tfstate-859217211731"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
