61c31b73f3f3a4bdcc15f672b80ff340c8239e76
Ohanyere Francis <mooref068@gmail.com>
2026-04-14 17:50:37 -0700

    chore: update Terraform backend bucket for dev environment

--- infra/terraform/environments/dev/backend.tf
+++ infra/terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "scholarship-mvp-tfstate-859217211731"
+    bucket       = "scholarship-mvp-tfstate-859217211731-use1"
     key          = "dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
